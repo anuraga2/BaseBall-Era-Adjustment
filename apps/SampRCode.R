84f5296e714163f0f1e300aebb01f316b1179bd2
anuraga2 <anuraga2@illinois.edu>
2021-07-20 00:35:26 -0500

    Removed Junk Files

--- apps/SampRCode.R
+++
@@ -1,2 +0,0 @@
-ran = data.frame(x1 = 1, x2 = 2)
-write.csv(ran,"samp_out.csv")
--- apps/test.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\R\R-4.0.3\bin\R.exe" CMD BATCH D:\UIUC\Era Adjustment\EraAdjustmentApp\MultiplePageApp\apps\SampRCode.R
